67e9227ae96442ecd237674fdcf81227c03601be
GerardoBatista <93355041+GerardoBatista@users.noreply.github.com>
2021-11-18 21:35:34 -0300

    Add files via upload

---
+++ proy 2.3/js/java.js
@@ -0,0 +1,14 @@
+
+alert("Boum Dia")
+
+
+//programa que pida dos numeros e indique cual es mayor
+alert("comparacion de numeros")
+let res1=prompt("Ingrese un Numero");
+let res2=prompt("Ingrese Otro Numero");
+if(res1<res2){
+    alert(res2)
+}
+else(res1>res2){
+    alert(res1)
+}
